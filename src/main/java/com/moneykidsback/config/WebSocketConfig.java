febc97183533115bf7421e39a5c2c70548fed889
Jeuk0908 <cell4086@naver.com>
2025-05-23 23:03:58 +0900

    chore: web socket 설정파일 수정

--- src/main/java/com/moneykidsback/config/WebSocketConfig.java
+++ src/main/java/com/moneykidsback/config/WebSocketConfig.java
@@ -1,29 +1,25 @@
 package com.moneykidsback.config;
 
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.*;
+
 
 @Configuration
-@EnableWebSocketMessageBroker
+@EnableWebSocketMessageBroker //웹소켓 메세지 브로커 클래스 선언
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-    // ① 클라이언트가 연결할 엔드포인트
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry
-                .addEndpoint("/stock-endpoint")        // FE가 이 URL로 SockJS 연결
-                .setAllowedOrigins("*")    // CORS 허용 (접근 가능한 도메인 설정) -> 배포 후 도메인 주소로 바꾸기
-                .withSockJS();             // SockJS fallback
-    }
 
-    // ② 메모리 브로커 설정 (간단한 사용)
+    // 1. 메세지 브로커 설정
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry
-                .enableSimpleBroker("/updates", "/alerts");  // 서버가 “푸시”용으로 쓰는 채널 prefix
-        registry
-                .setApplicationDestinationPrefixes("/send"); // 클라이언트가 서버 메소드 호출할 때 붙이는 prefix
+        registry.enableSimpleBroker("/updates", "/alerts");  // 서버 -> 클라이언트 방향 prefix(채널)
+        registry.setApplicationDestinationPrefixes("/send/stock-info"); // 클라이언트 -> 서버 방향 prefix
+    }
+
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS(); //웹소켓 엔드포인트
     }
 }
