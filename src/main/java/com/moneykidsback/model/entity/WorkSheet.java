1660a9b54216a076b90e4cb5d5212d406a43240c
cheeze37 <poker97.123.321.111.223@gmail.com>
2025-05-23 17:18:26 +0900

    경제 개념 ENTITY 요소 수정

---
+++ src/main/java/com/moneykidsback/model/entity/WorkSheet.java
@@ -0,0 +1,22 @@
+package com.moneykidsback.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity(name = "WorkSheet")
+public class WorkSheet {
+    @Id
+    @Column(name = "concept_id", nullable = false)
+    private int concept_id; // 개념 고유 식별 아이디
+
+    @Column(name = "difficulty")
+    private int difficulty; // 경제 개념 난이도 (1~5)
+
+    @Column(name = "title", length = 255, nullable = false)
+    private String title; // 개념 제목 ex) 수요
+
+    @Column(name = "content", columnDefinition = "TEXT", nullable = false)
+    private String content; // 개념 설명 ex) 수요란 물건을 구입하고자...
+
+}
