7dcd3369a9ab891c62cf5a0bc5480adc69f49cd3
G1Kimdev <g1kimdev@gmail.com>
2025-05-25 15:54:13 +0900

    [Test] StockRepository 함수 추가

--- src/main/java/com/moneykidsback/repository/StockRepository.java
+++ src/main/java/com/moneykidsback/repository/StockRepository.java
@@ -3,5 +3,15 @@ package com.moneykidsback.repository;
 import com.moneykidsback.model.entity.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StockRepository extends JpaRepository<Stock, String> {
+    // 주식코드로 종목 찾기
+    List<Stock> findByCode(String code);
+
+    // 종목 이름으로 종목 찾기
+    List<Stock> findByName(String name);
+
+    // 카테고리로 종목 찾기
+    List<Stock> findByCategory(String category);
 }
