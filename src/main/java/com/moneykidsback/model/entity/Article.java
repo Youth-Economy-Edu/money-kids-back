3b2527587760b677279b94327108fa66963e76c9
G1Kimdev <g1kimdev@gmail.com>
2025-05-28 12:33:01 +0900

    [Feat] 기사 엔티티 생성

---
+++ src/main/java/com/moneykidsback/model/entity/Article.java
@@ -0,0 +1,34 @@
+package com.moneykidsback.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Article {
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id; // 기사 ID
+
+    @Column(name = "stock_id")
+    private int stockId; // 대상 주식 ID
+
+    @Column(name = "title")
+    private String title; // 기사 제목
+
+    @Column(name = "date")
+    private String date; // 기사 날짜
+
+    @Column(name = "content")
+    private String content; // 기사 내용
+
+    @Column(name = "effect")
+    private String effect; // 주식에 미치는 영향
+}
