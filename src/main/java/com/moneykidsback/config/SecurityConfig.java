795c99321d9bcbf726ad9e395363410be23dc1e1
Jeuk0908 <cell4086@naver.com>
2025-05-30 15:05:00 +0900

    chore: SecurityConfig 추가

---
+++ src/main/java/com/moneykidsback/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.moneykidsback.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/stocks/trade/**").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(withDefaults())
+                .formLogin(withDefaults());
+
+        return http.build();
+    }
+
+    //테스트코드에선 인증 비활성화
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().anyRequest();
+    }
+
+    //테스트에서 기본 인증을 설정
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withUsername("testUser")
+                .password("{noop}testPassword")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
